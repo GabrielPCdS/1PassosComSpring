570eb1027daff4ed211730798dc7c085423132b5
Gabriel Pinheiro Cardoso da Silva <gabriel.pinheirocds@gmail.com>
2026-06-09 21:27:04 -0300

    feat: implementar mapeamento JPA em Produto e configurar banco de dados

--- src/main/java/com/github/gabrielpcdsi/ProdutosApi/controller/ProdutoController.java
+++ src/main/java/com/github/gabrielpcdsi/ProdutosApi/controller/ProdutoController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/produtos")
-public class PrudutoController {
+
+public class ProdutoController {
 
     @PostMapping
     public void salvar(@RequestBody Produto produto){
