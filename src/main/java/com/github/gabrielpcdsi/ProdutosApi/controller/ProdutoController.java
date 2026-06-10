c1943ebcf3626d362e433bf555be6e3be9439f87
Gabriel Pinheiro Cardoso da Silva <gabriel.pinheirocds@gmail.com>
2026-06-10 11:21:28 -0300

    feat: implementar endpoint de exclusao de produto

--- src/main/java/com/github/gabrielpcdsi/ProdutosApi/controller/ProdutoController.java
+++ src/main/java/com/github/gabrielpcdsi/ProdutosApi/controller/ProdutoController.java
@@ -32,5 +32,10 @@ public class ProdutoController {
         return produtoRepository.findById(id).orElse(null);
     }
 
+    @DeleteMapping("{id}")
+    public void deletar(@PathVariable("id") String id){
+        produtoRepository.deleteById(id);
+    }
+
 }
 
